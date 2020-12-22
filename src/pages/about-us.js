693ea295084c04eff775df3563697658d6437446
Laurent <loron@live.be>
2020-12-22 12:45:57 +0100

    les 7

---
+++ src/pages/about-us.js
@@ -0,0 +1,65 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import {
+  Wrapper,
+  Image,
+  BottomEdgeDown,
+  BottomEdgeUp,
+} from "../pages/pageStyles/pageStyles"
+import { COLORS } from "../constants"
+
+const AboutUsPage = () => {
+  const {
+    wpcontent: {
+      page: {
+        aboutUsMeta: { aboutUsPageDescription, aboutUsPageHeaderPicture },
+      },
+    },
+  } = useStaticQuery(graphql`
+    query {
+      wpcontent {
+        page(id: "about-us", idType: URI) {
+          aboutUsMeta {
+            aboutUsPageDescription
+            aboutUsPageHeaderPicture {
+              sourceUrl
+              imageFile {
+                childImageSharp {
+                  fluid(quality: 100) {
+                    ...GatsbyImageSharpFluid_withWebp
+                  }
+                }
+              }
+              altText
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <Layout>
+      <SEO title="About Us" />
+      <Wrapper descriptionColor={COLORS.PRIMARY}>
+        <div className="banner">
+          <Image
+            fluid={aboutUsPageHeaderPicture.imageFile.childImageSharp.fluid}
+            alt={aboutUsPageHeaderPicture.altText}
+          />
+          <BottomEdgeDown color={COLORS.PRIMARY} />
+        </div>
+        <div className="description">
+          <h2>About Us</h2>
+          <p>{aboutUsPageDescription}</p>
+          <BottomEdgeUp color={COLORS.BLACK} />
+        </div>
+      </Wrapper>
+    </Layout>
+  )
+}
+
+export default AboutUsPage
